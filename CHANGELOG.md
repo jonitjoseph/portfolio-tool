eb8efc7f0d9e0b8e3b2ca30a175f4a097f3f6be1
jonitjoseph <jonitjoseph@gmail.com>
2023-01-19 13:19:20 +0530

    v0.0.2-added axios

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,3 +2,8 @@
 ## version 0.0.1
 - added dummy CSV file with transactions
 - added function to calculate current portfolio value
+
+## version 0.0.2
+- added axios to fetch the API data
+- added function to fetch exchange rate for a given token
+- added function to derive the portfolio value from the exchange rate and portfolio token balance
--- index.js
+++ index.js
@@ -1,24 +1,55 @@
 const csv = require('csv-parser');
+const axios = require('axios')
 const fs = require('fs');
 
 const CSV_FILE = 'transactions.csv';
 
 let portfolio = {};
 
-// read the transactions from the CSV file and update the portfolio
-fs.createReadStream(CSV_FILE)
-    .pipe(csv())
-    .on('data', (data) => {
-        const { timestamp, transaction_type, token, amount } = data;
-        if (!portfolio[token]) {
-            portfolio[token] = 0;
-        }
-        if (transaction_type === 'DEPOSIT') {
-            portfolio[token] += parseFloat(amount);
-        } else if (transaction_type === 'WITHDRAWAL') {
-            portfolio[token] -= parseFloat(amount);
-        }
-    })
-    .on('end', () => {
-        console.log(portfolio);
-    });
+main().then(() => {
+    console.log('Thank You!');
+});
+
+getPortfolioValue('BTC')
+  .then((value) => {
+    console.log(value);
+  });
+
+async function main() {
+    // read the transactions from the CSV file and update the portfolio
+    fs.createReadStream(CSV_FILE)
+        .pipe(csv())
+        .on('data', (data) => {
+            const { timestamp, transaction_type, token, amount } = data;
+            if (!portfolio[token]) {
+                portfolio[token] = 0;
+            }
+            if (transaction_type === 'DEPOSIT') {
+                portfolio[token] += parseFloat(amount);
+            } else if (transaction_type === 'WITHDRAWAL') {
+                portfolio[token] -= parseFloat(amount);
+            }
+        })
+        .on('end', () => {
+            console.log(portfolio);
+        });
+}
+
+// function to get the exchange rate for a given token
+async function getExchangeRate(token) {
+    const response = await axios.get(`https://min-api.cryptocompare.com/data/price?fsym=${token}&tsyms=USD`);
+    console.log(token, response.data.USD);
+    return response.data.USD;
+};
+
+// function to get the portfolio value in USD for a given token
+async function getPortfolioValue(token) {
+    console.log(token);
+    const exchangeRate = await getExchangeRate(token);
+    let portfolioValueUSD = portfolio[token] * exchangeRate;
+    console.log(token, portfolioValueUSD);
+    return portfolioValueUSD;
+};
+
+// function to get the portfolio value in USD for a given token and date
+
